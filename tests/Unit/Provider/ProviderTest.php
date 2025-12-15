0b347f3a4efcd72c76c659c93e13923aa2a12bf4
Michael Dono <Michael.Dono2@networkrail.co.uk>
2025-12-15 13:30:19 +0000

    Added dummy test

---
+++ tests/Unit/Provider/ProviderTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace NetworkRailBusinessSystems\Common\Tests\Unit\Provider;
+
+use NetworkRailBusinessSystems\Common\Tests\TestCase;
+
+class ProviderTest extends TestCase
+{
+    public function test(): void {
+        $this->assertEquals(
+            'bob',
+            'bob'
+        );
+    }
+}
