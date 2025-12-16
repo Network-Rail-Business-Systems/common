465b7ae522731f839f18423ec6265f1511aa95d0
Tahreem Asghar <Tahreem.Asghar@networkrail.co.uk>
2025-12-16 09:15:00 +0000

    Run unit tests

--- tests/Unit/Provider/ProviderTest.php
+++ tests/Unit/Provider/ProviderTest.php
@@ -2,14 +2,45 @@
 
 namespace NetworkRailBusinessSystems\Common\Tests\Unit\Provider;
 
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
+use NetworkRailBusinessSystems\Common\CommonServiceProvider;
 use NetworkRailBusinessSystems\Common\Tests\TestCase;
+use Throwable;
 
 class ProviderTest extends TestCase
 {
-    public function test(): void {
-        $this->assertEquals(
-            'bob',
-            'bob'
-        );
+    protected CommonServiceProvider $provider;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->provider =new CommonServiceProvider($this->app);
+    }
+
+    public function testRedirectsWhenBaseUrl(): void
+    {
+        $exception = new HttpResponseException(redirect(config('common.home')));
+        $this->expectException($exception);
+        $this->expectExceptionObject($exception);
+
+        App::shouldReceive('runningInConsole')->andReturn(false);
+        URL::shouldReceive('getRequest->path')->andReturn('/');
+
+        $this->provider->boot();
+    }
+
+    public function test(): void
+    {
+        $this->expectNotToPerformAssertions();
+
+        App::shouldReceive('runningInConsole')->andReturn(false);
+        URL::shouldReceive('getRequest->path')->andReturn('/dashboard');
+
+        try {
+            $this->provider->boot();
+        } catch (Throwable $exception) {}
     }
 }
