cd505907b71c951cd9541de674001be1807a8ec4
Michael Dono <Michael.Dono2@networkrail.co.uk>
2025-12-17 11:56:41 +0000

    Seperated boot logic into seperate functions and completed code coverage

--- src/CommonServiceProvider.php
+++ src/CommonServiceProvider.php
@@ -18,35 +18,55 @@ class CommonServiceProvider extends ServiceProvider {
 
     public function boot(): void {
         $this->bootPublishes();
+        $this->redirectsBaseUrl();
+        $this->configureModels();
+        $this->checkHttps();
+    }
 
+    public function bootPublishes(): void
+    {
+        $this->publishes([
+            __DIR__ . '/config.php' => config_path('common.php'),
+        ], 'config');
+    }
+
+    public function redirectsBaseUrl(): void
+    {
         if (
             App::runningInConsole() === false
             && URL::getRequest()->path() === '/'
         ) {
-            throw new HttpResponseException(redirect(config('common.home')));
+            throw new HttpResponseException(
+                redirect(
+                    config('common.home')
+                )
+            );
         }
+    }
 
+    public function configureModels(): void
+    {
         Schema::defaultStringLength(191);
         Model::shouldBeStrict(App::environment() !== 'production');
+    }
 
-        // TODO: Find an performant way to make this more concise
+    public function checkHttps(): void
+    {
         if (
             in_array(Request::host(), [
+                'systems.networkrail.co.uk',
                 'systems.hiav.networkrail.co.uk',
+                'systems3.networkrail.co.uk',
                 'systems3.hiav.networkrail.co.uk',
+                'systems4.networkrail.co.uk',
+                'systems4.networkrail.co.uk',
                 'systems4.hiav.networkrail.co.uk',
+                'systems5.networkrail.co.uk',
                 'systems5.hiav.networkrail.co.uk'
             ])
         ) {
             URL::forceScheme('https');
             $this->app['request']->server->set('HTTPS', 'on');
         }
-    }
-
-    protected function bootPublishes(): void
-    {
-        $this->publishes([
-            __DIR__ . '/config.php' => config_path('common.php'),
-        ], 'config');
     }
 }
