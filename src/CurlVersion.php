23296cf792fa0e53752966254944bd79bee86600
Anthony Edmonds <anthony.edmonds@live.co.uk>
2026-08-05 09:17:31 +0100

    Fixed override

--- src/CurlVersion.php
+++ src/CurlVersion.php
@@ -2,6 +2,8 @@
 
 namespace NetworkRailBusinessSystems\Common;
 
+use Psr\Http\Message\RequestInterface;
+
 /**
  * The version of CURL on our servers is limited to an insecure build
  * Once the servers are updated past RHEL 7x, this should be removable
@@ -23,7 +25,32 @@ class CurlVersion
         return false;
     }
 
+    public static function supportsMultiplex(): bool
+    {
+        return false;
+    }
+
+    public static function supportsHttpVersionReuseMatching(): bool
+    {
+        return false;
+    }
+
+    public static function supportsRequiredHttp2Multiplex(): bool
+    {
+        return false;
+    }
+
     public static function supportsHttp2(): bool
+    {
+        return false;
+    }
+
+    public static function supportsHttp3(): bool
+    {
+        return false;
+    }
+
+    public static function supportsRequiredHttp3Multiplex(): bool
     {
         return false;
     }
@@ -53,6 +80,21 @@ class CurlVersion
         //
     }
 
+    public static function supportsConnectionSharing(): bool
+    {
+        return false;
+    }
+
+    public static function ensureConnectionSharingSupported(): void
+    {
+        //
+    }
+
+    public static function supportsShareConnectionCaches(): bool
+    {
+        return false;
+    }
+
     public static function supportsProxyTlsCredentialAwareConnectionReuse(): bool
     {
         return false;
@@ -63,12 +105,52 @@ class CurlVersion
         return false;
     }
 
+    public static function supportsSocksProxyCredentialAwareConnectionReuse(): bool
+    {
+        return false;
+    }
+
     public static function supportsProxyHeaderSeparation(): bool
     {
         return false;
     }
 
-    public static function getVersion(): ?string
+    public static function supportsProtocolsStr(): bool
+    {
+        return false;
+    }
+
+    public static function supportsProxyTunneling(): bool
+    {
+        return false;
+    }
+
+    public static function ensureSupported(RequestInterface $request): void
+    {
+        //
+    }
+
+    public static function supportsConnectionCaps(): bool
+    {
+        return false;
+    }
+
+    public static function ensureConnectionCapsSupported(string $option): void
+    {
+        //
+    }
+
+    public static function supportsRequiredMultiplex(): bool
+    {
+        return false;
+    }
+
+    public static function supportsNtlm(): bool
+    {
+        return false;
+    }
+
+    public static function getVersion(): string
     {
         return '7.29.0';
     }
